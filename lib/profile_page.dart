beab21138d309419b7d9dabf0f8d1237517c31c7
naduni123 <nadunidilmika@gmail.com>
2021-10-09 23:34:15 +0530

    profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -200,3 +200,42 @@ Widget appbar(BuildContext context){
     ),
   );
 }
+showConfirmDialog(
+    BuildContext context,
+    String title,
+    String description,
+    String confirmBtnTxt,
+    String cancelBtnTxt,
+    Function onConfirmClicked,
+    ) {
+  // set up the buttons
+  Widget cancelButton = TextButton(
+    child: Text(cancelBtnTxt),
+    onPressed: () {
+      Navigator.of(context).pop(); // dismiss dialog
+    },
+  );
+  Widget confirmButton = TextButton(
+    child: Text(confirmBtnTxt),
+    onPressed: () {
+      onConfirmClicked.call();
+      Navigator.of(context).pop(); // dismiss dialog
+    },
+  );
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text(title),
+    content: Text(description),
+    actions: [
+      cancelButton,
+      confirmButton,
+    ],
+  );
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
